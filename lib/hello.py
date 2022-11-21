6d99fc0f32b01f90a9ded2f76f314383deff92d1
Paul John <rainson.work@gmail.com>
2022-11-21 16:56:26 +0300

    Added math functionality

--- lib/hello.py
+++ lib/hello.py
@@ -3,3 +3,10 @@
 # Default name World
 name = 'World'
 print(f'Hello, {name}')
+
+#let's see how this works
+print('Enter two numbers to get thier product')
+num1 = int(input('Enter the first number:'))
+num2 = int(input('Enter the second number:'))
+
+print(f"{num1} times {num2} we get", num1 * num2)
